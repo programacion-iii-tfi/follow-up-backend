4f4b4b5203bf547c8f4975629d2371a25d40235e
Pablo <pablo.and.ramirez@outlook.com>
2026-07-05 13:36:44 -0300

    [FIX] se elige no correr las migraciones mediante docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,5 @@ sed -i "s/listen 8080/listen ${PORT:-8080}/" /etc/nginx/http.d/default.conf
 
 php artisan config:cache
 php artisan route:cache
-php artisan migrate --force
 
 exec supervisord -c /etc/supervisord.conf
